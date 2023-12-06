272ae33ba7e5690f5ec3038b8b32789cbc088123
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2023-12-06 11:34:55 -0600

    Getting Conor started on ProductPage component

--- frontend/src/components/ProductPage/ProductPage.js
+++ frontend/src/components/ProductPage/ProductPage.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const ProductPage = () => (
-  <div>
-    ProductPage Component
-  </div>
-);
+const ProductPage = ({product}) => {
+  const [productId, brand, price, title, text, image, reviews] = product;
+  // Reviews JSON Array: reviews[0].userId, reviews[0].title, reviews[0].text, reviews[0].rating
+  // User name will be fetched later from the user id.
+
+  return (
+    <div>
+      ProductPage Component
+    </div>
+  );
+};
 
 ProductPage.propTypes = {};
 
